59da06791dfcc58b0371e1e71a59e9428c418d11
Lorinczy Zsigmond <zsigmond.lorinczy@idomsoft.hu>
2025-06-17 14:34:55 +0200

    Added setsockopt(SOL_SOCKET, SO_KEEPALIVE)

--- src/net.c
+++ src/net.c
@@ -31,6 +31,10 @@ void setSocketDefaults(SOCKET fd) {
 	int tmp = 1024;
 	setsockopt(fd, SOL_SOCKET, SO_SNDBUF, &tmp, sizeof(tmp));
 #endif
+	{
+		int tmp= 1;
+		setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &tmp, sizeof(tmp));
+	}
 }
 
 int getAddrInfoWithProto(char *address, char *port, int protocol, struct addrinfo **ai)
